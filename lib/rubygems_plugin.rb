d74b6e894c99383188022031596c68e24eec4be5
Vít Ondruch <vondruch@redhat.com>
2012-10-15 11:19:38 +0200

    Use pre_install hook instead of new command.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,2 +1,13 @@
-require "rubygems/command_manager"
-Gem::CommandManager.instance.register_command :nice_install
+Gem.pre_install do |gem_installer|
+  unless gem_installer.spec.extensions.empty?
+    have_tools = %w{gcc make sh}.all? do |t|
+      system("#{t} --version > NUL 2>&1")
+    end
+
+    unless have_tools
+      raise Gem::InstallError,<<-EOT
+                           The '#{gem_installer.spec.name}' native gem requires installed build tools.
+      EOT
+    end
+  end
+end
