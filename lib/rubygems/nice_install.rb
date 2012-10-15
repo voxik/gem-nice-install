0a1e800b24a6c8ae41fa2f1110d9b1c9b8add22e
Vít Ondruch <vondruch@redhat.com>
2012-10-15 14:50:29 +0200

    Try to install dependencies when extension build fails.

--- lib/rubygems/nice_install.rb
+++ lib/rubygems/nice_install.rb
@@ -2,20 +2,40 @@ require 'rubygems/nice_install/base_ext_installer'
 require 'rubygems/nice_install/fedora_ext_installer'
 
 module Gem
-  ext_installer = FedoraExtInstaller.new
 
   pre_install do |gem_installer|
     unless gem_installer.spec.extensions.empty?
-      missing_deps = ext_installer.gem_ext_dependencies_for(gem_installer.spec.name).delete_if do |t|
-        ext_installer.ext_dependency_present?(t)
+      gem_installer.extend Gem::Installer::Nice
+    end
+  end
+
+end
+
+
+module Gem
+  class Installer
+    module Nice
+      def build_extensions
+        super
+      rescue ExtensionBuildError => e
+        # Install platform dependencies and try the build again.
+        install_platform_dependencies
+        super
       end
 
-      unless missing_deps.empty?
-        unless ext_installer.install_ext_dependencies_for(gem_installer.spec.name, missing_deps)
-          raise Gem::InstallError, "Failed to install native dependencies for '#{gem_installer.spec.name}'."
+      def install_platform_dependencies
+        ext_installer = FedoraExtInstaller.new
+
+        missing_deps = ext_installer.gem_ext_dependencies_for(spec.name).delete_if do |t|
+          ext_installer.ext_dependency_present?(t)
+        end
+
+        unless missing_deps.empty?
+          unless ext_installer.install_ext_dependencies_for(spec.name, missing_deps)
+            raise Gem::InstallError, "Failed to install native dependencies for '#{spec.name}'."
+          end
         end
       end
     end
   end
-
 end
