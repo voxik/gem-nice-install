e0ed74ee39520e6e721b0f95239a9d5456a71987
Bohuslav Kabrda <bkabrda@redhat.com>
2012-10-15 11:29:16 +0200

    Move the logic to lib/rubygems/nice_install.rb

--- lib/rubygems/nice_install.rb
+++ lib/rubygems/nice_install.rb
@@ -1,8 +1,13 @@
-class Gem::NiceInstall
-  def initialize(gemfile)
-    @gemfile = gemfile
+Gem.pre_install do |gem_installer|
+  unless gem_installer.spec.extensions.empty?
+  have_tools = %w{gcc make sh}.all? do |t|
+    system("#{t} --version > /dev/null 2>&1")
   end
 
-  def install
+  unless have_tools
+    raise Gem::InstallError, <<-EOT
+The '#{gem_installer.spec.name}' native gem requires installed build tools.
+    EOT
+    end
   end
 end
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,13 +1,1 @@
-Gem.pre_install do |gem_installer|
-  unless gem_installer.spec.extensions.empty?
-    have_tools = %w{gcc make sh}.all? do |t|
-      system("#{t} --version > NUL 2>&1")
-    end
-
-    unless have_tools
-      raise Gem::InstallError,<<-EOT
-                           The '#{gem_installer.spec.name}' native gem requires installed build tools.
-      EOT
-    end
-  end
-end
+require 'rubygems/nice_install'
